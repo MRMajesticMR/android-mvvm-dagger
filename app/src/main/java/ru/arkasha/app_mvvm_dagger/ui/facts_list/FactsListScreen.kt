9ac83ee3cb6ce46eefec53517df18244eb39eb4a
arkadiy.zakharov <arkadiy,zakharov@doconcall.ru>
2021-07-03 14:12:53 +0300

    Migrate to DataBinding

--- app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsListScreen.kt
+++ app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsListScreen.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import kotlinx.android.synthetic.main.f_facts_list.*
 import ru.arkasha.app_mvvm_dagger.R
 import ru.arkasha.app_mvvm_dagger.base.recycler_view.tuneVertical
+import ru.arkasha.app_mvvm_dagger.databinding.FFactsListBinding
 
 class FactsListScreen : Fragment(R.layout.f_facts_list) {
 
     private val viewModel: FactsListScreenViewModel by viewModels()
 
+    private var binding: FFactsListBinding? = null
+
     private val factsRecyclerViewAdapter = FactsRecyclerViewAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        rvFacts.tuneVertical(factsRecyclerViewAdapter)
+        binding = FFactsListBinding.bind(view)
+
+        binding?.rvFacts?.tuneVertical(factsRecyclerViewAdapter)
 
         viewModel.uiCatsFacts.observe(viewLifecycleOwner, { facts ->
             factsRecyclerViewAdapter.setData(facts)
@@ -24,24 +28,32 @@ class FactsListScreen : Fragment(R.layout.f_facts_list) {
         viewModel.uiContentState.observe(viewLifecycleOwner, { state ->
             when (state) {
                 ContentState.LOADING -> {
-                    tvLoading?.visibility = View.VISIBLE
-                    rvFacts?.visibility = View.GONE
-                    tvError?.visibility = View.GONE
+                    binding?.tvLoading?.visibility = View.VISIBLE
+                    binding?.rvFacts?.visibility = View.GONE
+                    binding?.tvError?.visibility = View.GONE
                 }
 
                 ContentState.CONTENT -> {
-                    tvLoading?.visibility = View.GONE
-                    rvFacts?.visibility = View.VISIBLE
-                    tvError?.visibility = View.GONE
+                    binding?.tvLoading?.visibility = View.GONE
+                    binding?.rvFacts?.visibility = View.VISIBLE
+                    binding?.tvError?.visibility = View.GONE
                 }
 
                 ContentState.ERROR -> {
-                    tvLoading?.visibility = View.GONE
-                    rvFacts?.visibility = View.GONE
-                    tvError?.visibility = View.VISIBLE
+                    binding?.tvLoading?.visibility = View.GONE
+                    binding?.rvFacts?.visibility = View.GONE
+                    binding?.tvError?.visibility = View.VISIBLE
                 }
+
+                else -> throw IllegalStateException("Illegal state: $state")
             }
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding = null
     }
 
 }
--- app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsRecyclerViewAdapter.kt
+++ app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsRecyclerViewAdapter.kt
@@ -1,20 +1,32 @@
 package ru.arkasha.app_mvvm_dagger.ui.facts_list
 
-import kotlinx.android.synthetic.main.i_cat_fact.view.*
+import androidx.recyclerview.widget.RecyclerView
 import ru.arkasha.app_mvvm_dagger.R
 import ru.arkasha.app_mvvm_dagger.base.recycler_view.SingleViewHolderRecyclerViewAdapter
 import ru.arkasha.app_mvvm_dagger.data.facts.CatFact
+import ru.arkasha.app_mvvm_dagger.databinding.ICatFactBinding
 
 class FactsRecyclerViewAdapter : SingleViewHolderRecyclerViewAdapter<CatFact>() {
+
+    private var binding: ICatFactBinding? = null
 
     override val viewHolderLayoutId: Int = R.layout.i_cat_fact
 
     override fun bindModel(holder: ViewHolder, model: CatFact) {
         with(holder.itemView) {
-            tvDate.text = model.creationDate
-            tvText.text = model.title
+            binding = ICatFactBinding.bind(this)
 
-            setOnClickListener {  }
+            binding?.tvDate?.text = model.creationDate
+            binding?.tvText?.text = model.title
+
+            setOnClickListener { }
         }
     }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+
+        binding = null
+    }
+
 }
