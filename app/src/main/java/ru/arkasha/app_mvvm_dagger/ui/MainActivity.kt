effdf37e727e1572a21175b1c216fbe37c910bd1
A.Zakharov <mrmajesticmr90@gmail.com>
2021-07-08 12:33:18 +0300

    Add navigation component

--- app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/MainActivity.kt
+++ app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/MainActivity.kt
@@ -1,15 +1,6 @@
 package ru.arkasha.app_mvvm_dagger.ui
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import ru.arkasha.app_mvvm_dagger.R
-import ru.arkasha.app_mvvm_dagger.ui.facts_list.FactsListScreen
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
--- app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsListScreenViewModel.kt
+++ app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/facts_list/FactsListScreenViewModel.kt
@@ -3,12 +3,7 @@ package ru.arkasha.app_mvvm_dagger.ui.facts_list
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.orhanobut.logger.Logger
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
-import ru.arkasha.app_mvvm_dagger.api.ApisProvider
 import ru.arkasha.app_mvvm_dagger.api.ApisProviderImpl
 import ru.arkasha.app_mvvm_dagger.data.facts.CatFact
 import ru.arkasha.app_mvvm_dagger.data.facts.CatFactFromCatFactCatsApiObjectMapper
@@ -26,8 +21,6 @@ class FactsListScreenViewModel : ViewModel() {
     )
 
     init {
-        Logger.d("FactsListScreenViewModel created!")
-
         loadCatFacts()
     }
 
@@ -36,19 +29,13 @@ class FactsListScreenViewModel : ViewModel() {
             uiContentState.value = ContentState.LOADING
 
             try {
-                uiCatsFacts.value  = factsRepository.getFacts()
+                uiCatsFacts.value = factsRepository.getFacts()
 
                 uiContentState.value = ContentState.CONTENT
             } catch (e: Throwable) {
                 uiContentState.value = ContentState.ERROR
             }
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-
-        Logger.d("FactsListScreenViewModel destroyed!")
     }
 
 }
