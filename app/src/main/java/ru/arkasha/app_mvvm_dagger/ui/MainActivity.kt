34351e309006f5091829e0b0d5b853d1e940f776
A.Zakharov <mrmajesticmr90@gmail.com>
2021-08-14 00:23:32 +0300

    Enable edge-to-edge mode and set up status bar color

--- app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/MainActivity.kt
+++ app/src/main/java/ru/arkasha/app_mvvm_dagger/ui/MainActivity.kt
@@ -1,6 +1,21 @@
 package ru.arkasha.app_mvvm_dagger.ui
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import ru.arkasha.app_mvvm_dagger.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdgeMode()
+    }
+
+    private fun enableEdgeToEdgeMode() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+}
